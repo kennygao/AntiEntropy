5a150f57b09d4a41ec79315f738d993aca30e6b3
Kenny Gao <kennytgao@gmail.com>
2012-03-22 01:19:20 -0400

    Implemented naive replica synchronization scheme with network traffic cost benchmarking.

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+from Replica import Replica
+from NaiveReplica import NaiveReplica
 from MerkleReplica import MerkleReplica
 
 # init parameters
@@ -10,16 +12,13 @@ import random
 
 # test pairwise synchronization
 testdata = {}
-testdata['a'] = map(lambda d, t: MerkleReplica.Datum(d, t), map(str, random.sample(range(maxvalue), datasize)), random.sample(range(maxvalue), datasize))
+testdata['a'] = map(lambda d, t: Replica.Datum(d, t), map(str, random.sample(range(maxvalue), datasize)), random.sample(range(maxvalue), datasize))
 testdata['b'] = list(testdata['a'])
 
 conflicts = random.randint(0, datasize)
 # conflicts = 0
 for x in random.sample(range(datasize), conflicts):
-    testdata['b'][x] = MerkleReplica.Datum(random.randrange(maxvalue), random.randrange(maxvalue))
-
-a = MerkleReplica(testdata['a'])
-b = MerkleReplica(testdata['b'])
+    testdata['b'][x] = Replica.Datum(random.randrange(maxvalue), random.randrange(maxvalue))
 
 print('Data size: {}.'.format(datasize))
 print('Maximum data value: {}.'.format(maxvalue))
@@ -28,20 +27,35 @@ print('Number of conflicts: {}.'.format(conflicts))
 print('')
 
 print('--- Replica 1:')
-print(a)
+print(testdata['a'])
 print('--- Replica 2:')
-print(b)
+print(testdata['b'])
 
 print('')
 
-print('--- Replica 1 Merkle Tree:')
-a.computemerkle()
-a.printmerkle()
-print('--- Replica 2 Merkle Tree:')
-b.computemerkle()
-b.printmerkle()
+a = NaiveReplica(list(testdata['a']))
+b = NaiveReplica(list(testdata['b']))
+
+indices = a.synchronize(b)
 
 print('')
+
+print('Replica 1 and Replica 2 were synchronized at indices {} ({} total).'.format(indices, len(indices)))
+print('Network traffic cost: {}.'.format(a.networktraffic))
+
+print('')
+
+a = MerkleReplica(list(testdata['a']))
+b = MerkleReplica(list(testdata['b']))
+
+# print('--- Replica 1 Merkle Tree:')
+# a.computemerkle()
+# a.printmerkle()
+# print('--- Replica 2 Merkle Tree:')
+# b.computemerkle()
+# b.printmerkle()
+
+# print('')
 
 indices = a.synchronize(b)
 
---
+++ NaiveReplica.py
@@ -0,0 +1,26 @@
+from Replica import Replica
+
+class NaiveReplica(Replica):
+    # initialize data
+    def __init__(self, data=[]):
+        # assumption (without loss of generality): data length must be power of 2
+        self.data = data
+        self.networktraffic = 0
+
+    def synchronize(self, origin):
+        indices = []
+        for index in range(len(self.data)):
+            self.networktraffic += 1
+            if self.data[index].datum != origin.data[index].datum:
+                print('   Resolving conflict at index {}.'.format(index))
+                indices.append(index)
+                self.resolveconflict(origin, index)
+        print('   Conflict resolution complete.')
+        return indices
+
+    def resolveconflict(self, origin, index):
+        if self.data[index].timestamp < origin.data[index].timestamp:
+            self.data[index] = origin.data[index]
+        else:
+            self.networktraffic += 1
+            origin.data[index] = self.data[index]
