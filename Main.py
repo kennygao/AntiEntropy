653ec3d607f32892077715a745f6a7296627b300
Kenny Gao <kennytgao@gmail.com>
2012-03-23 04:21:05 -0400

    Implemented benchmark runner and trimmed output to contain only aggregate results.

--- Main.py
+++ Main.py
@@ -3,65 +3,45 @@ from NaiveReplica import NaiveReplica
 from MerkleReplica import MerkleReplica
 
 # init parameters
-datasize = 16
 maxvalue = 1000
+datacount = 32
+testcount = 100
 
 ##########
 
 import random
 
-# test pairwise synchronization
-testdata = {}
-testdata['a'] = map(lambda d, t: Replica.Datum(d, t), map(str, random.sample(range(maxvalue), datasize)), random.sample(range(maxvalue), datasize))
-testdata['b'] = list(testdata['a'])
-
-conflicts = random.randint(0, datasize)
-# conflicts = 0
-for x in random.sample(range(datasize), conflicts):
-    testdata['b'][x] = Replica.Datum(random.randrange(maxvalue), random.randrange(maxvalue))
-
-print('Data size: {}.'.format(datasize))
+print('Data count: {}.'.format(datacount))
 print('Maximum data value: {}.'.format(maxvalue))
 print('Number of conflicts: {}.'.format(conflicts))
 
-print('')
+for conflicts in range(datacount):
+    networktraffic = {}
+    networktraffic['naive'] = []
+    networktraffic['merkle'] = []
 
-# print('--- Replica 1:')
-# print(testdata['a'])
-# print('--- Replica 2:')
-# print(testdata['b'])
+    for _ in range(testcount):
+        testdata = {}
+        testdata['a'] = map(lambda d, t: Replica.Datum(d, t), map(str, random.sample(range(maxvalue), datacount)), random.sample(range(maxvalue), datacount))
+        testdata['b'] = list(testdata['a'])
 
-# print('')
+        for x in random.sample(range(datacount), conflicts):
+            testdata['b'][x] = Replica.Datum(random.randrange(maxvalue), random.randrange(maxvalue))
 
-a = NaiveReplica(list(testdata['a']))
-b = NaiveReplica(list(testdata['b']))
+        a = NaiveReplica(list(testdata['a']))
+        b = NaiveReplica(list(testdata['b']))
 
-print('--- Naive scheme:')
+        indices = a.synchronize(b)
 
-indices = a.synchronize(b)
+        # print('Naive scheme cost: {}.'.format(a.networktraffic))
+        networktraffic['naive'].append(a.networktraffic)
 
-print('Replica 1 and Replica 2 were synchronized at indices {} ({} total).'.format(indices, len(indices)))
-print('Network traffic cost: {}.'.format(a.networktraffic))
+        a = MerkleReplica(list(testdata['a']))
+        b = MerkleReplica(list(testdata['b']))
 
-print('')
+        indices = a.synchronize(b)
 
-a = MerkleReplica(list(testdata['a']))
-b = MerkleReplica(list(testdata['b']))
+        # print('Merkle tree-based scheme cost: {}.'.format(a.networktraffic))
+        networktraffic['merkle'].append(a.networktraffic)
 
-# print('--- Replica 1 Merkle Tree:')
-# a.computemerkle()
-# a.printmerkle()
-# print('--- Replica 2 Merkle Tree:')
-# b.computemerkle()
-# b.printmerkle()
-
-# print('')
-
-print('--- Merkle tree-based scheme:')
-
-indices = a.synchronize(b)
-
-print('Replica 1 and Replica 2 were synchronized at indices {} ({} total).'.format(indices, len(indices)))
-print('Network traffic cost: {}.'.format(a.networktraffic))
-
-print('')
+    print([sum(networktraffic[n], 0.0) / testcount for n in networktraffic])
